44b08bc800a66f4d3eed8abb0ed430adea257bc2
Dani Cosme <dani.cosme@gmail.com>
2017-10-17 22:16:43 -0700

    removed variable definition

--- extract_timecourses.m
+++ extract_timecourses.m
@@ -10,7 +10,9 @@
 %   * marsbar-0.44
 %
 % Inputs:
-%   * ROI(s) must have been extraced from marsbar and save as .mat files. ROI names cannot include dashes(-) and must have the following pattern *_roi.mat
+%   * ROI(s) must have been extraced from marsbar and save as .mat
+%   files. ROI names cannot include dashes(-) and must have the
+%   following pattern *_roi.mat
 % 
 % Output: 
 %   * Structure $timecourses saved to $output_dir as a .mat file
@@ -20,22 +22,12 @@
 % http://marsbar.sourceforge.net/marsbar.pdf
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Specify variables
-% Add paths
+%% Add paths
 addpath('/Users/danicosme/Documents/MATLAB/spm12');
 addpath(genpath('/Users/danicosme/Documents/MATLAB/spm12/toolbox/marsbar-0.44'));
 
-% Define variables
-subjects = {'FP001', 'FP002', 'FP003', 'FP004', 'FP005', 'FP006', 'FP007', 'FP008', 'FP009', 'FP010', 'FP011', 'FP012', 'FP013', 'FP014', 'FP015', 'FP016', 'FP018', 'FP019', 'FP020', 'FP021', 'FP022', 'FP023', 'FP024', 'FP025', 'FP026', 'FP027', 'FP028', 'FP029', 'FP030', 'FP031', 'FP032', 'FP034', 'FP035'}; %Add subjects IDs here
-rois = {'RL_CBLM_roi', 'RL_DLPFC_roi', 'RL_IFG_roi', 'RL_SFG_roi', 'RL_SMA_roi'};
-fir_length = 20; % length of FIR in seconds
-tr_length = 2;
-        
-% Define paths
-sub_dir = '/Volumes/psych-cog/dsnlab/FP/BDX_univariate/subjects';
-roi_dir = '/Volumes/psych-cog/dsnlab/FP/BDX_univariate/ROI/marsbar';
-fx_file = 'fx/fx_standard/SPM.mat';
-output_dir = '/Volumes/psych-cog/dsnlab/FP/BDX_univariate/timecourses';
+%% Source config file
+config
 
 %% Loop through subjects and ROIs and extract time courses
 for i = 1:length(subjects)
