e2eda07d42c8369e13610e4f4dc44749f5a5345e
Dani Cosme <dani.cosme@gmail.com>
2017-10-18 10:44:33 -0700

    tidied code, fixed structure error

--- extract_timecourses.m
+++ extract_timecourses.m
@@ -18,7 +18,7 @@
 %   * Structure $timecourses saved to $output_dir as a .mat file
 %   * Table $datatable saved to $output_dir as a .csv file
 % 
-% More information about the marsbar code can be found here: 
+% Marsbar code borrowed form this document: 
 % http://marsbar.sourceforge.net/marsbar.pdf
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -43,29 +43,38 @@ for i = 1:length(subjects)
         
         % Make marsbar design object
         D = mardo(spm_name);
+        
         % Make marsbar ROI object
         R = maroi(roi_file);
+        
         % Fetch data into marsbar data object
         Y = get_marsy(R, D, 'mean');
+        
         % Get contrasts from original design
         xCon = get_contrasts(D);
+        
         % Estimate design on ROI data
         E = estimate(D, Y);
+        
         % Put contrasts from original design back into design object
         E = set_contrasts(E, xCon);
-        % get design betas
+        
+        % Get design betas
         b = betas(E);
+        
         % get stats and stuff for all contrasts into statistics structure
         marsS = compute_contrasts(E, 1:length(xCon));
 
         % Get compound event types structure
         ets = event_types_named(E);
         n_event_types = length(ets);
+        
         % Bin size in seconds for FIR
         bin_size = tr(E);
 
         % Number of FIR time bins to cover length of FIR
         bin_no = fir_length / bin_size;
+        
         % Options - here 'single' FIR model, return estimated % signal change
         opts = struct('single', 1, 'percent', 1);
         for e_t = 1:n_event_types
@@ -75,7 +84,7 @@ for i = 1:length(subjects)
 
         % Save in structure
         timecourses.(char(sub)).(char(roi)).tc = fir_tc;
-        timecourses.(char(sub)).(char(roi)).events = ets.name;
+        timecourses.(char(sub)).(char(roi)).events = ets;
         
         % Clear variables
         clearvars -except sub roi timecourses subjects rois fir_length sub_dir roi_dir fx_file i j
@@ -99,6 +108,7 @@ for i = 1:numel(subjects)
             roi = table(repmat(rois(j),ntr,1), 'VariableNames', {'roi'});
             tr = table([1:ntr]', 'VariableNames', {'tr'});
             tc = table(timecourses.(char(subjects(i))).(char(rois(j))).tc(:,(k)), 'VariableNames', {'percentSignal'});
+            
             % Concatenate all tables into one 'DataTable'
             subTable = horzcat(sub, cond, roi, tr, tc);
             datatable = vertcat(datatable, subTable);
